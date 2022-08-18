31c35779e62156499a1d1ea94ea01cdfa893380d
vasil-yordanov14 <vasil_sh@abv.bg>
2022-08-18 18:31:49 +0300

    prev and next btns

--- src/Buttons.js
+++ src/Buttons.js
@@ -1,8 +1,31 @@
-import React from 'react'
-import { useGlobalContext } from './context'
+import React from "react";
+import { useGlobalContext } from "./context";
 
 const Buttons = () => {
-  return <h2>button container</h2>
-}
+  const { isLoading, page, nbPages, handlePage } = useGlobalContext();
+  return (
+    <div className="btn-container">
+      <button
+        disabled={isLoading}
+        onClick={() => {
+          handlePage(`dec`);
+        }}
+      >
+        prev
+      </button>
+      <p>
+        {page + 1} of {nbPages}
+      </p>
+      <button
+        disabled={isLoading}
+        onClick={() => {
+          handlePage(`inc`);
+        }}
+      >
+        next
+      </button>
+    </div>
+  );
+};
 
-export default Buttons
+export default Buttons;
